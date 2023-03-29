5c4dee415cadde9b3f41ecfce1f862bcc5569bbe
amieldanao <amielrenaissance4@gmail.com>
2023-03-29 19:06:44 +0800

    for checking revision

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -41,7 +41,10 @@ const routes: Routes = [
       },
       {
         path: 'map',
-        loadChildren: () => import('../map/map.module').then(m => m.MapModule)
+        loadChildren: () => import('../map/map.module').then(m => m.MapModule),
+        data:{
+          reuserComponent: false
+        }
       },
       {
         path: '',
